3b11940e83bf2ea064ddf6af684e62b8a7e24922
Pablo <pp-pjimenez@zonar.cl>
2026-09-23 00:37:28 -0300

    se hicieron pequeños ajustes

--- config/cognito.ts
+++ config/cognito.ts
@@ -3,6 +3,6 @@ export const COGNITO_CONFIG = {
   clientId: '3f6ndcnegj9ufuurqhet3soeme',
   region: 'us-east-1',
   userPoolId: 'us-east-1_ZoKCVBrHG',
-  redirectUri: 'nexo://redirect',
+  redirectUri: 'nexoapp://redirect',
   scopes: ['openid', 'email', 'profile'],
 };
